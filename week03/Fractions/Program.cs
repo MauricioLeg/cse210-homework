bccea2f723d65451195a2ed4faf8495537f0e8c5
Mauricio Leguizamon <leguizamonmauricio@gmail.com>
2025-11-14 19:29:12 -0300

    Fraction activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+        
+        fraction1.SetTop(3);
+        fraction2.SetTop(1);
+        fraction2.SetBottom(3);
+        fraction3.SetBottom(3);
+
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
     }
 }
