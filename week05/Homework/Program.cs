0ee1dda75a11b6d4c9f74208fbecf2767dc06c2b
Mauricio Leguizamon <leguizamonmauricio@gmail.com>
2025-11-25 18:02:42 -0300

    Inheritance Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
